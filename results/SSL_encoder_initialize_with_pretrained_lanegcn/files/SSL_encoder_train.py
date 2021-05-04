c649b42cb0f1fe70bfeb919e6dc7b68567f55750
HyeongseokJeon 5900x <521hsgugu@kaist.ac.kr>
2021-05-05 05:47:29 +0900

    asdf

--- results/SSL_encoder_initialize_with_pretrained_lanegcn/files/SSL_encoder_train.py
+++ results/SSL_encoder_initialize_with_pretrained_lanegcn/files/SSL_encoder_train.py
@@ -207,7 +207,7 @@ def train(epoch, config, config_enc, train_loader, net, loss, opt, val_loader=No
         loss_tot = loss_tot + loss_out.item()
         loss_calc = loss_calc + 1
         lr = opt.step(epoch)
-
+        print(loss_out)
         if torch.isnan(loss_out):
             print('output')
             print(output)
