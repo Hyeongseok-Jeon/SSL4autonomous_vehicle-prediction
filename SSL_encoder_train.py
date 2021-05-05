f0fe1ec81b39c264bcf0cdde757e8df0c0a95c81
HyeongseokJeon 5900x <521hsgugu@kaist.ac.kr>
2021-05-06 02:33:51 +0900

    asdf

--- SSL_encoder_train.py
+++ SSL_encoder_train.py
@@ -27,7 +27,7 @@ from torch.utils.data.distributed import DistributedSampler
 from LaneGCN.utils import Logger, load_pretrain
 
 from mpi4py import MPI
-torch.autograd.detect_anomaly(True)
+torch.autograd.set_detect_anomaly(True)
 comm = MPI.COMM_WORLD
 hvd.init()
 torch.cuda.set_device(hvd.local_rank())
