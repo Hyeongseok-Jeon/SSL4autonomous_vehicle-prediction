96bad68ffc3148003f093defc3c843191d44ca2b
HyeongseokJeon 5900x <521hsgugu@kaist.ac.kr>
2021-05-08 04:45:25 +0900

    asdf

--- SSL_encoder_train.py
+++ SSL_encoder_train.py
@@ -149,7 +149,7 @@ def main():
     train_loader = DataLoader(
         dataset,
         batch_size=config['batch_size'],
-        num_workers=config["workers"],
+        num_workers=8,
         sampler=train_sampler,
         collate_fn=collate_fn,
         pin_memory=True,
