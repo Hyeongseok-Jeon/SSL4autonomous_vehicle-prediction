99efb758e7d72db164c8588006f3ffd9377bde10
HyeongseokJeon 5900x <521hsgugu@kaist.ac.kr>
2021-05-12 04:32:23 +0900

    sadf

--- 1_lanegcn_train.py
+++ 1_lanegcn_train.py
@@ -52,7 +52,7 @@ parser.add_argument(
     "--weight", default="", type=str, metavar="WEIGHT", help="checkpoint path"
 )
 parser.add_argument(
-    "--memo", default="_1mods"
+    "--memo", default="_6mods"
 )
 parser.add_argument("--mode", default='client')
 parser.add_argument("--port", default=52162)
