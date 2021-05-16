ac63f17e1da09bc4bd339ff1d167fbc1c4673acb
HyeongseokJeon 5900x <521hsgugu@kaist.ac.kr>
2021-05-17 02:12:06 +0900

    sadf

--- 3_ActionConditional_lanegcn_train.py
+++ 3_ActionConditional_lanegcn_train.py
@@ -52,7 +52,7 @@ parser.add_argument(
     "--weight", default="", type=str, metavar="WEIGHT", help="checkpoint path"
 )
 parser.add_argument(
-    "--memo", default="_11mods"
+    "--memo", default="_6mods"
 )
 parser.add_argument(
     "--encoder", default="encoder_1"
