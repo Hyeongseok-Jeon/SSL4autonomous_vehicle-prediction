7b2c7e8394a314fc75e1652e79d98882963415c3
jhs <521hsgugu@kaist.ac.kr>
2021-05-20 02:59:35 +0900

    asdf

---
+++ results/3_ActionConditional_lanegcn_6mods_action_hid_seperated_leakyrelu_activation_encoder_2/files/3_ActionConditional_lanegcn.py
@@ -0,0 +1,448 @@
+# Copyright (c) 2020 Uber Technologies, Inc.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import numpy as np
+import os
+import sys
+sys.path.extend(['/home/jhs/Desktop/SSL4autonomous_vehicle-prediction/LaneGCN'])
+sys.path.extend(['/home/ubuntu/VDC/HyeongseokJeon/SSL4autonomous_vehicle-prediction/LaneGCN'])
+sys.path.extend(['/home/user/data/HyeongseokJeon/SSL4autonomous_vehicle-prediction/LaneGCN'])
+from fractions import gcd
+from numbers import Number
+from importlib import import_module
+import torch
+from torch import Tensor, nn
+from torch.nn import functional as F
+
+from data import ArgoDataset, collate_fn
+from utils import gpu, to_long,  Optimizer, StepLR
+
+from layers import Conv1d, Res1d, Linear, LinearRes, Null
+from numpy import float64, ndarray
+from typing import Any, Callable, Dict, List, Optional, Tuple, Type, Union
+
+
+file_path = os.path.abspath(__file__)
+# file_path = os.getcwd()
+root_path = os.path.dirname(file_path)
+model_name = os.path.basename(file_path).split(".")[0]
+
+### config ###
+config = dict()
+"""Train"""
+config["display_iters"] = 205942
+config["val_iters"] = 205942 * 2
+config["save_freq"] = 1.0
+config["epoch"] = 0
+config["horovod"] = True
+config["opt"] = "adam"
+config["num_epochs"] = 50
+config["lr"] = [1e-3, 1e-4]
+config["lr_epochs"] = [32]
+config["lr_func"] = StepLR(config["lr"], config["lr_epochs"])
+
+
+if "save_dir" not in config:
+    config["save_dir"] = os.path.join(
+        root_path, "results", model_name
+    )
+
+if not os.path.isabs(config["save_dir"]):
+    config["save_dir"] = os.path.join(root_path, "results", config["save_dir"])
+
+config["batch_size"] = 64
+config["val_batch_size"] = 64
+config["workers"] = 0
+config["val_workers"] = config["workers"]
+
+
+"""Dataset"""
+# Raw Dataset
+config["train_split"] = os.path.join(
+    root_path, "dataset/train/data"
+)
+config["val_split"] = os.path.join(root_path, "dataset/val/data")
+config["test_split"] = os.path.join(root_path, "dataset/test_obs/data")
+
+# Preprocessed Dataset
+config["preprocess"] = True # whether use preprocess or not
+config["preprocess_train"] = os.path.join(
+    root_path, "LaneGCN", "dataset","preprocess", "train_crs_dist6_angle90_mod.p"
+)
+config["preprocess_val"] = os.path.join(
+    root_path, "LaneGCN", "dataset", "preprocess", "val_crs_dist6_angle90_mod.p"
+)
+config['preprocess_test'] = os.path.join(root_path, "dataset",'preprocess', 'test_test.p')
+
+"""Model"""
+config["rot_aug"] = False
+config["pred_range"] = [-100.0, 100.0, -100.0, 100.0]
+config["num_scales"] = 6
+config["n_actor"] = 128
+config["n_map"] = 128
+config["actor2map_dist"] = 7.0
+config["map2actor_dist"] = 6.0
+config["actor2actor_dist"] = 100.0
+config["pred_size"] = 30
+config["pred_step"] = 1
+config["num_preds"] = config["pred_size"] // config["pred_step"]
+config["num_mods"] = 6
+config["cls_coef"] = 1.0
+config["reg_coef"] = 1.0
+config["mgn"] = 0.2
+config["cls_th"] = 2.0
+config["cls_ignore"] = 0.2
+### end of config ###
+
+class Net(nn.Module):
+    """
+    Lane Graph Network contains following components:
+        1. ActorNet: a 1D CNN to process the trajectory input
+        2. MapNet: LaneGraphCNN to learn structured map representations
+           from vectorized map data
+        3. Actor-Map Fusion Cycle: fuse the information between actor nodes
+           and lane nodes:
+            a. A2M: introduces real-time traffic information to
+                lane nodes, such as blockage or usage of the lanes
+            b. M2M:  updates lane node features by propagating the
+                traffic information over lane graphs
+            c. M2A: fuses updated map features with real-time traffic
+                information back to actors
+            d. A2A: handles the interaction between actors and produces
+                the output actor features
+        4. PredNet: prediction header for motion forecasting using
+           feature from A2A
+    """
+    def __init__(self, config, baseline, encoder):
+        super(Net, self).__init__()
+        self.config = config
+        self.baseline = baseline
+        self.encoder = encoder
+        self.actor_net = baseline.ActorNet(config).cuda()
+        self.map_net = baseline.MapNet(config).cuda()
+
+        self.a2m = baseline.A2M(config).cuda()
+        self.m2m = baseline.M2M(config).cuda()
+        self.m2a = baseline.M2A(config).cuda()
+        self.a2a = baseline.A2A(config).cuda()
+
+        self.action_emb = encoder.encoder(config).cuda()
+        self.pred_net = PredNet(config).cuda()
+        self.pred_net_second = PredNet(config).cuda()
+
+    def forward(self, data: Dict) -> Dict[str, List[Tensor]]:
+        # construct actor feature
+        actors, actor_idcs = self.baseline.actor_gather(gpu(data["feats"]))
+        actor_ctrs = gpu(data["ctrs"])
+        actors = self.actor_net(actors)
+
+        # construct map features
+        graph = self.baseline.graph_gather(to_long(gpu(data["graph"])))
+        nodes, node_idcs, node_ctrs = self.map_net(graph)
+
+        # actor-map fusion cycle
+        nodes = self.a2m(nodes, graph, actors, actor_idcs, actor_ctrs)
+        nodes = self.m2m(nodes, graph)
+        actors = self.m2a(actors, actor_idcs, actor_ctrs, nodes, node_idcs, node_ctrs)
+        actors = self.a2a(actors, actor_idcs, actor_ctrs)
+
+        target_idx = torch.cat([x[1].unsqueeze(dim=0) for x in actor_idcs])
+        actors = actors[target_idx]
+        actor_ctrs = [actor_ctrs[i][1:2] for i in range(len(actor_ctrs))]
+        actor_idcs = []
+        count = 0
+        for i in range(len(actor_ctrs)):
+            idcs = torch.arange(count, count + 1).to(actors.device)
+            actor_idcs.append(idcs)
+            count += 1
+        # prediction
+        out = self.pred_net(actors, actor_idcs, actor_ctrs)
+        rot, orig = gpu(data["rot"]), gpu(data["orig"])
+        # transform prediction to world coordinates
+        for i in range(len(out["reg"])):
+            out["reg"][i] = torch.matmul(out["reg"][i], rot[i]) + orig[i].view(
+                1, 1, 1, -1
+            )
+
+        conditional_actors = self.action_emb(actors, data, out)
+        out_react = self.pred_net_second(conditional_actors, actor_idcs, actor_ctrs)
+        # transform prediction to world coordinates
+
+        out_final = dict()
+        out_final['cls'] = out['cls']
+        out_final['reg'] = [out['reg'][i] + out_react['reg'][i] for i in range(len(out['reg']))]
+        return out_final
+
+
+class PredNet(nn.Module):
+    """
+    Final motion forecasting with Linear Residual block
+    """
+    def __init__(self, config):
+        super(PredNet, self).__init__()
+        self.config = config
+        norm = "GN"
+        ng = 1
+
+        n_actor = config["n_actor"]
+
+        pred = []
+        for i in range(config["num_mods"]):
+            pred.append(
+                nn.Sequential(
+                    nn.Linear(n_actor, 2 * config["num_preds"]),
+                )
+            )
+        self.pred = nn.ModuleList(pred)
+
+        self.att_dest = AttDest(n_actor)
+        self.cls = nn.Sequential(
+            LinearRes(n_actor, n_actor, norm=norm, ng=ng), nn.Linear(n_actor, 1)
+        )
+
+    def forward(self, actors: Tensor, actor_idcs: List[Tensor], actor_ctrs: List[Tensor]) -> Dict[str, List[Tensor]]:
+        preds = []
+        for i in range(len(self.pred)):
+            preds.append(self.pred[i](actors))
+        reg = torch.cat([x.unsqueeze(1) for x in preds], 1)
+        reg = reg.view(reg.size(0), reg.size(1), -1, 2)
+
+        for i in range(len(actor_idcs)):
+            idcs = actor_idcs[i]
+            ctrs = actor_ctrs[i].view(-1, 1, 1, 2)
+            reg[idcs] = reg[idcs] + ctrs
+
+        dest_ctrs = reg[:, :, -1].detach()
+        feats = self.att_dest(actors, torch.cat(actor_ctrs, 0), dest_ctrs)
+        cls = self.cls(feats).view(-1, self.config["num_mods"])
+
+        cls, sort_idcs = cls.sort(1, descending=True)
+        row_idcs = torch.arange(len(sort_idcs)).long().to(sort_idcs.device)
+        row_idcs = row_idcs.view(-1, 1).repeat(1, sort_idcs.size(1)).view(-1)
+        sort_idcs = sort_idcs.view(-1)
+        reg = reg[row_idcs, sort_idcs].view(cls.size(0), cls.size(1), -1, 2)
+
+        out = dict()
+        out["cls"], out["reg"] = [], []
+        for i in range(len(actor_idcs)):
+            idcs = actor_idcs[i]
+            ctrs = actor_ctrs[i].view(-1, 1, 1, 2)
+            out["cls"].append(cls[idcs])
+            out["reg"].append(reg[idcs])
+        return out
+
+
+class AttDest(nn.Module):
+    def __init__(self, n_agt: int):
+        super(AttDest, self).__init__()
+        norm = "GN"
+        ng = 1
+
+        self.dist = nn.Sequential(
+            nn.Linear(2, n_agt),
+            nn.ReLU(inplace=True),
+            Linear(n_agt, n_agt, norm=norm, ng=ng),
+        )
+
+        self.agt = Linear(2 * n_agt, n_agt, norm=norm, ng=ng)
+
+    def forward(self, agts: Tensor, agt_ctrs: Tensor, dest_ctrs: Tensor) -> Tensor:
+        n_agt = agts.size(1)
+        num_mods = dest_ctrs.size(1)
+
+        dist = (agt_ctrs.unsqueeze(1) - dest_ctrs).view(-1, 2)
+        dist = self.dist(dist)
+        agts = agts.unsqueeze(1).repeat(1, num_mods, 1).view(-1, n_agt)
+
+        agts = torch.cat((dist, agts), 1)
+        agts = self.agt(agts)
+        return agts
+
+
+class PredLoss(nn.Module):
+    def __init__(self, config):
+        super(PredLoss, self).__init__()
+        self.config = config
+        self.reg_loss = nn.SmoothL1Loss(reduction="sum")
+
+    def forward(self, out: Dict[str, List[Tensor]], gt_preds: List[Tensor], has_preds: List[Tensor]) -> Dict[str, Union[Tensor, int]]:
+        cls, reg = out["cls"], out["reg"]
+        cls = torch.cat([x for x in cls], 0)
+        reg = torch.cat([x for x in reg], 0)
+        gt_preds = torch.cat([x for x in gt_preds], 0)
+        has_preds = torch.cat([x for x in has_preds], 0)
+
+        loss_out = dict()
+        zero = 0.0 * (cls.sum() + reg.sum())
+        loss_out["cls_loss"] = zero.clone()
+        loss_out["num_cls"] = 0
+        loss_out["reg_loss"] = zero.clone()
+        loss_out["num_reg"] = 0
+
+        num_mods, num_preds = self.config["num_mods"], self.config["num_preds"]
+        # assert(has_preds.all())
+
+        last = has_preds.float() + 0.1 * torch.arange(num_preds).float().to(
+            has_preds.device
+        ) / float(num_preds)
+        max_last, last_idcs = last.max(1)
+        mask = max_last > 1.0
+
+        cls = cls[mask]
+        reg = reg[mask]
+        gt_preds = gt_preds[mask]
+        has_preds = has_preds[mask]
+        last_idcs = last_idcs[mask]
+
+        row_idcs = torch.arange(len(last_idcs)).long().to(last_idcs.device)
+        dist = []
+        for j in range(num_mods):
+            dist.append(
+                torch.sqrt(
+                    (
+                        (reg[row_idcs, j, last_idcs] - gt_preds[row_idcs, last_idcs])
+                        ** 2
+                    ).sum(1)
+                )
+            )
+        dist = torch.cat([x.unsqueeze(1) for x in dist], 1)
+        min_dist, min_idcs = dist.min(1)
+        row_idcs = torch.arange(len(min_idcs)).long().to(min_idcs.device)
+
+        mgn = cls[row_idcs, min_idcs].unsqueeze(1) - cls
+        mask0 = (min_dist < self.config["cls_th"]).view(-1, 1)
+        mask1 = dist - min_dist.view(-1, 1) > self.config["cls_ignore"]
+        mgn = mgn[mask0 * mask1]
+        mask = mgn < self.config["mgn"]
+        coef = self.config["cls_coef"]
+        loss_out["cls_loss"] += coef * (
+            self.config["mgn"] * mask.sum() - mgn[mask].sum()
+        )
+        loss_out["num_cls"] += mask.sum().item()
+
+        reg = reg[row_idcs, min_idcs]
+        coef = self.config["reg_coef"]
+        loss_out["reg_loss"] += coef * self.reg_loss(
+            reg[has_preds], gt_preds[has_preds]
+        )
+        loss_out["num_reg"] += has_preds.sum().item()
+        return loss_out
+
+
+class Loss(nn.Module):
+    def __init__(self, config):
+        super(Loss, self).__init__()
+        self.config = config
+        self.pred_loss = PredLoss(config)
+
+    def forward(self, out: Dict, data: Dict) -> Dict:
+        batch_num = len(data['city'])
+        loss_out = self.pred_loss(out, [gpu(data["gt_preds"])[i][1:2] for i in range(batch_num)], [gpu(data["has_preds"])[i][1:2] for i in range(batch_num)])
+        loss_out["loss"] = loss_out["cls_loss"] / (
+            loss_out["num_cls"] + 1e-10
+        ) + loss_out["reg_loss"] / (loss_out["num_reg"] + 1e-10)
+        return loss_out
+
+
+class PostProcess(nn.Module):
+    def __init__(self, config):
+        super(PostProcess, self).__init__()
+        self.config = config
+
+    def forward(self, out,data):
+        post_out = dict()
+        post_out["preds"] = [x[0:1].detach().cpu().numpy() for x in out["reg"]]
+        post_out["gt_preds"] = [x[1:2].numpy() for x in data["gt_preds"]]
+        post_out["has_preds"] = [x[1:2].numpy() for x in data["has_preds"]]
+        return post_out
+
+    def append(self, metrics: Dict, loss_out: Dict, post_out: Optional[Dict[str, List[ndarray]]]=None) -> Dict:
+        if len(metrics.keys()) == 0:
+            for key in loss_out:
+                if key != "loss":
+                    metrics[key] = 0.0
+            for key in post_out:
+                metrics[key] = []
+
+        for key in loss_out:
+            if key == "loss":
+                continue
+            if isinstance(loss_out[key], torch.Tensor):
+                metrics[key] += loss_out[key].item()
+            else:
+                metrics[key] += loss_out[key]
+
+        for key in post_out:
+            metrics[key] += post_out[key]
+
+        return metrics
+
+    def display(self, metrics, dt, epoch, lr=None):
+        """Every display-iters print training/val information"""
+        if lr is not None:
+            print("Epoch %3.3f, lr %.5f, time %3.2f" % (epoch, lr, dt))
+        else:
+            print(
+                "************************* Validation, time %3.2f *************************"
+                % dt
+            )
+
+        cls = metrics["cls_loss"] / (metrics["num_cls"] + 1e-10)
+        reg = metrics["reg_loss"] / (metrics["num_reg"] + 1e-10)
+
+        loss = cls + reg
+
+        preds = np.concatenate(metrics["preds"], 0)
+        gt_preds = np.concatenate(metrics["gt_preds"], 0)
+        has_preds = np.concatenate(metrics["has_preds"], 0)
+        ade1, fde1, ade, fde, min_idcs = pred_metrics(preds, gt_preds, has_preds)
+
+        print(
+            "loss %2.4f %2.4f %2.4f, ade1 %2.4f, fde1 %2.4f, ade %2.4f, fde %2.4f"
+            % (loss, cls, reg, ade1, fde1, ade, fde)
+        )
+        print()
+
+
+def pred_metrics(preds, gt_preds, has_preds):
+    assert has_preds.all()
+    preds = np.asarray(preds, np.float32)
+    gt_preds = np.asarray(gt_preds, np.float32)
+
+    """batch_size x num_mods x num_preds"""
+    err = np.sqrt(((preds - np.expand_dims(gt_preds, 1)) ** 2).sum(3))
+
+    ade1 = err[:, 0].mean()
+    fde1 = err[:, 0, -1].mean()
+
+    min_idcs = err[:, :, -1].argmin(1)
+    row_idcs = np.arange(len(min_idcs)).astype(np.int64)
+    err = err[row_idcs, min_idcs]
+    ade = err.mean()
+    fde = err[:, -1].mean()
+    return ade1, fde1, ade, fde, min_idcs
+
+
+def get_model(args):
+    encoder = import_module('ActionEncoders.' + args.encoder)
+    baseline = import_module('LaneGCN.lanegcn')
+
+    net = Net(config, baseline, encoder)
+    net = net.cuda()
+    #
+    # pre_trained_weight = torch.load(root_path + "/LaneGCN/pre_trained" + '/36.000.ckpt')
+    # pretrained_dict = pre_trained_weight['state_dict']
+    # new_model_dict = net.state_dict()
+    # pretrained_dict = {k: v for k, v in pretrained_dict.items() if k in new_model_dict}
+    # new_model_dict.update(pretrained_dict)
+    # net.load_state_dict(new_model_dict)
+
+    loss = Loss(config).cuda()
+    post_process = PostProcess(config).cuda()
+
+    params = net.parameters()
+    opt = Optimizer(params, config)
+
+
+    return config, ArgoDataset, collate_fn, net, loss, post_process, opt
