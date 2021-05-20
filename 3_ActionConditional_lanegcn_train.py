19c2e78040533c6c22a093362bd1dd7a5170989e
HyeongseokJeon 5900x <521hsgugu@kaist.ac.kr>
2021-05-20 16:13:36 +0900

    asdf

--- 3_ActionConditional_lanegcn_train.py
+++ 3_ActionConditional_lanegcn_train.py
@@ -3,7 +3,7 @@
 # limitations under the License.
 
 import os
-os.environ["CUDA_VISIBLE_DEVICES"]="2, 3"
+# os.environ["CUDA_VISIBLE_DEVICES"]="2, 3"
 os.umask(0)
 os.environ["MKL_NUM_THREADS"] = "1"
 os.environ["NUMEXPR_NUM_THREADS"] = "1"
